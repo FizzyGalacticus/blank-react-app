e4910ad6b4658088bb5115bae06a53f008445afc
Dustin Dodson <dustinleedodson@gmail.com>
2019-02-13 17:42:56 -0800

    Gulp v4 (#1)
    * Upgraded to gulp-v4

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import store from './lib/redux/store';
+
+import App from './App';
+
+const app = document.getElementById('app');
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	app
+);
